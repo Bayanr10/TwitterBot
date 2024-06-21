9dfdbd4ecc5acb5f3910829b76e21c463de2c495
Bayanr10 <bayansfakeemail@gmail.com>
2024-06-21 17:22:05 -0400

    Created function to initialize all authorizations for Discord and Twitter

--- commands.py
+++ commands.py
@@ -1,7 +1,5 @@
 from aiohttp import ClientSession
 
-url = 'https://api.twitter.com/1.1/statuses/update.json'
-
 async def update(message):
     if message.attachments:
                 img = message.attachments[0]
@@ -17,11 +15,20 @@ async def update(message):
                         await message.channel.send('server pic updated!')
                 else:
                     await message.channel.send('incorrect file type, please use a jpg, jpeg, or png file')
+
+
 async def handle_tweet(message, twit_client):
     tweet_content = message.content[len('!tweet'):]
     try:
-        twit_client.create_tweet(text=tweet_content)
-        await message.channel.send('Tweet posted!')
-        print(f"Tweet Successful: {tweet_content}")
+        if message.attachments:
+            media = message.attachments[0]
+            media_id = message.url
+            twit_client.create_tweet(text=tweet_content, media_ids=media_id)
+            print(f"Tweet Successful: {tweet_content}")
+        else:    
+            twit_client.create_tweet(text=tweet_content)
+            await message.channel.send('Tweet posted!')
+            print(f"Tweet Successful: {tweet_content}")
     except Exception as e:
+        await message.channel.send(f'Error: {e}')
         print(f"Error Occured: {e}")
