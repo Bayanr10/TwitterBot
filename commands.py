cba25c0ff6d7bfc0f1fd6ff9f8c816a9da3abfcb
Bayanr10 <bayansfakeemail@gmail.com>
2024-06-22 13:43:35 -0400

    created count_reactions() function to update yes/no variables to realtime counts

--- commands.py
+++ commands.py
@@ -1,7 +1,7 @@
 from aiohttp import ClientSession
 import requests
 import os
-import time
+import asyncio
 
 async def update(message):
     if message.attachments:
@@ -23,33 +23,63 @@ async def add_react(message):
     await message.add_reaction('✅')
     await message.add_reaction('❌')
 
+def count_reactions(message):
+    yes = 0
+    no = 0
+    for reaction in message.reactions:
+        print(f"reaction is {reaction}")
+        if (reaction == '✅'):
+            yes = reaction.count
+        elif (reaction == '❌'):
+            no = reaction.count
+    return yes, no
+
+
 async def handle_tweet(message, twit_client, api):
     tweet_content = message.content[len('!tweet'):]
     try:
+        await add_react(message)
         if message.attachments:
             img = message.attachments[0]
             if img.filename.endswith(('.jpg', '.png', '.jpeg', '.gif')):
                 response = requests.get(img.url)
                 file_path = f'tmp_{img.filename}'
                 with open(file_path, 'wb') as f:
-                    #f.write(response.content)
-                    #media = api.media_upload(file_path)
-                    #media_id = media.media_id_string
-                    #twit_client.create_tweet(text=tweet_content, media_ids=[media_id])
-                    #await message.channel.send('Tweet posted!')
-                    await add_react(message)
+                    f.write(response.content)
+                    media = api.media_upload(file_path)
+                    media_id = media.media_id_string
+                    yes, no = count_reactions(message)
+                    limit = 0
+                    while ((yes < 2 and no < 2) and limit != 20):
+                        asyncio.sleep(5)
+                        yes, no = count_reactions(message)
+                        ++limit
+                    if (yes < no):
+                        await message.channel.send('tweet not posted')
+                    else:
+                        #twit_client.create_tweet(text=tweet_content, media_ids=[media_id])
+                        #await message.channel.send('Tweet posted!')
+                        os.remove(file_path)
+                        await message.channel.send('tweet posted')
                     
                     print(f"Tweet Successful: {tweet_content}")
-        else:    
-            #twit_client.create_tweet(text=tweet_content)
-            await message.channel.send('Tweet posted!')
-            print(f"Tweet Successful: {tweet_content}")
-            await add_react(message)
+        else:
+            yes, no = count_reactions(message)
+            limit = 0
+            while ((yes < 2 and no < 2) and limit != 20):
+                asyncio.sleep(5)
+                yes, no = count_reactions(message)
+                limit += 1
+                print(f"limit is {limit}")
+                print(f"yes is {yes}\nno is {no}")
+            if (yes < no):
+                await message.channel.send('tweet not posted')
+            else:
+                #twit_client.create_tweet(text=tweet_content)
+                print(f"Tweet Successful: {tweet_content}")
+                await message.channel.send('tweet posted')    
 
 
     except Exception as e:
         await message.channel.send(f'Error: {e}')
         print(f"Error Occured: {e}")
-    
-    #finally:
-    #    os.remove(file_path)
