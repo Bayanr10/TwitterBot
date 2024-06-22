368e5f5e694f7b4d70bc2ee5a6538c1a54857018
Bayanr10 <bayansfakeemail@gmail.com>
2024-06-22 13:05:02 -0400

    Added reactions to messages started with !tweet

--- commands.py
+++ commands.py
@@ -1,6 +1,7 @@
 from aiohttp import ClientSession
 import requests
 import os
+import time
 
 async def update(message):
     if message.attachments:
@@ -18,6 +19,9 @@ async def update(message):
                 else:
                     await message.channel.send('incorrect file type, please use a jpg, jpeg, or png file')
 
+async def add_react(message):
+    await message.add_reaction('✅')
+    await message.add_reaction('❌')
 
 async def handle_tweet(message, twit_client, api):
     tweet_content = message.content[len('!tweet'):]
@@ -28,25 +32,24 @@ async def handle_tweet(message, twit_client, api):
                 response = requests.get(img.url)
                 file_path = f'tmp_{img.filename}'
                 with open(file_path, 'wb') as f:
-                    f.write(response.content)
-                try:
-                    media = api.media_upload(file_path)
-                    media_id = media.media_id_string
-                    twit_client.create_tweet(text=tweet_content, media_ids=[media_id])
-                    await message.channel.send('Tweet posted!')
+                    #f.write(response.content)
+                    #media = api.media_upload(file_path)
+                    #media_id = media.media_id_string
+                    #twit_client.create_tweet(text=tweet_content, media_ids=[media_id])
+                    #await message.channel.send('Tweet posted!')
+                    await add_react(message)
+                    
                     print(f"Tweet Successful: {tweet_content}")
-                except Exception as e:
-                    await message.channel.send(f'Error: {e}')
-                    print(f"Error Occured: {e}")
         else:    
-            twit_client.create_tweet(text=tweet_content)
+            #twit_client.create_tweet(text=tweet_content)
             await message.channel.send('Tweet posted!')
             print(f"Tweet Successful: {tweet_content}")
+            await add_react(message)
 
 
     except Exception as e:
         await message.channel.send(f'Error: {e}')
         print(f"Error Occured: {e}")
     
-    finally:
-         os.remove(file_path)
+    #finally:
+    #    os.remove(file_path)
